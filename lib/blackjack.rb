f6648b1a522712ebfe1dc3764fa01bb43a5974fb
Andy Choi <wooseokchoi96@gmail.com>
2019-08-06 04:29:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,6 +57,7 @@ def runner
   current_total = initial_round
   until current_total > 21
     current_total = hit?(current_total)
+    display_card_total(current_total)
   end
 end_game(current_total)
 end
